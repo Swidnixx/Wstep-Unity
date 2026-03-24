e27f2820802a7156dc2876a7d8fc302ed32ff58d
Fitables <snupodeusz@gmail.com>
2026-03-24 13:29:43 +0100

    Zmiana sceny w projekcie i skryptu do skoku
    Usunięcie CameraScript ponieważ nie jest potrzebny gdy kamera jest child'em gracza. Uproszczenie skryptu skoku, gracz może skoczyć tylko raz przed dotknięciem ponownie podłogi (lub boku platformy przez to jak działa OnCollisionEnter2D). Dodanie kilka elementów do sceny.

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -1,14 +1,26 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public float jumpForce = 7f;
+    private bool isGrounded=false;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) &&  isGrounded)
         {
-            rb.AddForce(Vector2.up * 300);
+          rb.linearVelocity = new Vector2(rb.linearVelocity.x,jumpForce);
+          isGrounded=false;
         }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+        void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 }
